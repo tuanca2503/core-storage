93215e8a022e46e33ea73fad85800830248b238a
tuanca2503 <tuan.ca.2503@gmail.com>
2026-07-12 06:55:01 +0700

    move test to home

--- crates/core-storage/examples/test.rs
+++ crates/core-storage/examples/test.rs
@@ -1,5 +1,19 @@
-use core::hello;
+use sysinfo::Disks;
+
 //cargo run -p core --example test
 pub fn main() {
-    hello();
+    let disks = Disks::new_with_refreshed_list();
+    for disk in disks.list() {
+        println!("Name      : {:?}", disk.name());
+        println!("Mount     : {:?}", disk.mount_point());
+        println!("Total     : {}", disk.total_space());
+        println!("Available : {}", disk.available_space());
+        println!("Kind      : {:?}", disk.kind());
+        println!("FS        : {:?}", disk.file_system());
+        println!("is_removable        : {:?}", disk.is_removable());
+        println!("is_read_only        : {:?}", disk.is_read_only());
+
+
+        println!();
+    }
 }
--- crates/core-storage/src/lib.rs
+++ crates/core-storage/src/lib.rs
@@ -1,17 +1,8 @@
+
 pub mod store;
 pub mod error;
 
 
 pub fn hello() {
-    let disks = Disks::new_with_refreshed_list();
-    for disk in disks.list() {
-        println!("Name      : {:?}", disk.name());
-        println!("Mount     : {:?}", disk.mount_point());
-        println!("Total     : {}", disk.total_space());
-        println!("Available : {}", disk.available_space());
-        println!("Kind      : {:?}", disk.kind());
-        println!("FS        : {:?}", disk.file_system());
-        println!();
-    }
     println!("Hello, world!");
 }
